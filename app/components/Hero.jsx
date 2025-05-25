2a4328218a1c8f6cd6e551c39e3348e22e01806e
Aashish Shah <shahaashih@gmail.com>
2025-05-25 08:12:18 +0545

    updated hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,11 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
-    const backImage=[""]
+    const backImage = ["/Hero/food_banner_1.jpg", "/Hero/food_banner_2.jpg", "/Hero/food_banner_3.jpg", "/Hero/food_banner_4.jpg", "/Hero/food_banner_5.jpg"]
     return (
-        <div className='container my-4'>
-
-
+        <div className='container my-4 h-[85vh] relative'>
+            <div className='absolute inset-0 h-full'>
+                <Image
+                    src="/Hero/food_banner_1.jpg"
+                    width={1000}
+                    height={1000}
+                    alt='Hero Image'
+                    className='h-full w-full object-cover object-center rounded-lg'
+                />
+            </div>
         </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 
 import Header from "./reuseComponents/Header"
+import Hero from './components/Hero'
 export default function Home() {
   return (
     <>
       <div className="container  h-screen">
         <Header />
-      </div>
+        <Hero />
+      </div >
     </>
   );
 }
