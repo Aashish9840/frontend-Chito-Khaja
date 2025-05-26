d41da0e5d8e9026c23ab18d14d40086b4cc6fdd4
Aashish Shah <shahaashih@gmail.com>
2025-05-26 09:43:34 +0545

    add to cart design

--- app/components/FoodList.jsx
+++ app/components/FoodList.jsx
@@ -78,24 +78,29 @@ const FoodList = () => {
             {
                 successFoodList?.length > 0 ?
 
-                    <div className='grid my-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 h-fit w-full' id="foodlist">
+                    <div className='grid my-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 h-fit w-full' id="foodlist">
                         {successFoodList?.map((food, index) => (
-                            <div className='flex py-2 flex-col gap-2 border-bulk-white border-2 shadow-md transition-all transform duration-300 ease-in hover:scale-105 rounded-md px-4' key={index}>
-                                <Image
-                                    src={`${imagePath}/${food?.image}`}
-                                    height={200}
-                                    width={200}
-                                    alt="food image"
-                                    className="h-[250px] w-[100%] border rounded-md my-2"
-                                />
+                            <div className='flex py-2 flex-col gap-2 border-bulk-white border-2 shadow-md rounded-md px-4' key={index}>
+
+                                <div className='h-[250px] w-[100%] overflow-hidden'>
+                                    <Image
+                                        src={`${imagePath}/${food?.image}`}
+                                        height={200}
+                                        width={200}
+                                        alt="food image"
+                                        className="h-full w-[100%] border rounded-md transition-all transform duration-300 ease-in hover:scale-105"
+                                    />
+                                </div>
+
                                 <section>
                                     <h1 className='text-xl font-dm_sans font-medium'>{food.name}</h1>
                                 </section>
-                                <h1 className='text-[16px] text-gray-700 font-dm_sans'>{`${food.description.split(" ").slice(0, 7).join(" ")}...`}</h1>
+                                <h1 className='text-[14px] text-gray-700 font-dm_sans'>{`${food.description.split(" ").slice(0, 8).join(" ")}...`}</h1>
                                 <section className='flex gap-12 items-center'>
                                     <h1 className='text-[18px] font-semibold text-gray-800 font-dm_sans'>Rs {food.prize}</h1>
-                                    <h2 className='text-[16px] font-dm_sans text-gray-700'>Discount by 2%</h2>
+                                    <h2 className='text-[14px] font-dm_sans text-gray-700'>Discount by 2%</h2>
                                 </section>
+                                <button className='w-full bg-blue-700 text-white py-2 rounded-md text-center font-medium hover:bg-blue-600'>Add To Cart</button>
                             </div>
                         ))}
 
