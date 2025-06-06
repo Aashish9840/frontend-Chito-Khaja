8e50e3aaacc5f108eba37652d378344d50855806
Aashish Shah <shahaashih@gmail.com>
2025-06-06 22:04:12 +0545

    updated payment integration

--- app/reuseComponents/Header.jsx
+++ app/reuseComponents/Header.jsx
@@ -173,7 +173,7 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="#797070" d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z" /></svg>
                     {validateUser && validateUser?.cardData?.length >= 0 &&
 
-                        <div className='absolute bg-red-400 p-1 top-[-15] rounded-[50%] w-[20px] h-[20px] flex items-center justify-center left-4'>
+                        <div className='absolute bg-red-400 p-1 top-[-10px] rounded-[50%] w-[20px] h-[20px] flex items-center justify-center left-4'>
 
 
                             <h1 className=' font-extrabold font-dm_sans text-sm text-white'>{validateUser.cardData.length}</h1>
