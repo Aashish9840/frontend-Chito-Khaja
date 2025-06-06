7e1df4d417452c9f60b73afbf5adc28581f85eee
Aashish Shah <shahaashih@gmail.com>
2025-06-06 23:48:19 +0545

    changes in esewa

---
+++ app/esewa/success/[id]/page.jsx
@@ -0,0 +1,59 @@
+'use client'
+import { useParams, useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { errorToast } from '../../../reuseComponents/ReactToast'
+
+const page = () => {
+    const params = useParams();
+    const orderId = params?.id;
+    const router = useRouter()
+    const [orderInfo, setOrderInfo] = useState(null)
+    const [errorOrderInfo, setErrorOrderInfo] = useState(null)
+    console.log(orderId, "orderId")
+    const getOrderDetails = async (orderId) => {
+        try {
+            setErrorOrderInfo(null)
+            setOrderInfo(null)
+            const request = await fetch(`/api/payment/esewa/successInformation/${orderId}`, {
+                methodL: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            const info = await request.json()
+            if (request?.ok) {
+                setOrderInfo(info.data)
+            }
+            else {
+                setErrorOrderInfo(info.message)
+            }
+        }
+        catch (error) {
+            setErrorOrderInfo(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if (orderId) {
+            getOrderDetails(orderId)
+        }
+
+    }, [orderId])
+
+    useEffect(() => {
+        if (orderInfo) {
+            router.push('/')
+        }
+        if (errorOrderInfo
+
+        ) {
+            errorToast(errorOrderInfo)
+        }
+    }, [orderInfo, errorOrderInfo])
+
+    return (
+        <></>
+    )
+}
+
+export default page
