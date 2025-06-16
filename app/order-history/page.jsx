8bd21769ea7730cd4708847b18666f33770df06d
Aashish Shah <shahaashih@gmail.com>
2025-06-16 10:04:08 +0545

    date filer in order history

--- app/order-history/page.jsx
+++ app/order-history/page.jsx
@@ -4,14 +4,22 @@ import Header from '../reuseComponents/Header'
 import Footer from '../components/Footer'
 import useGetOrderList from '../services/users/useGetOrderList'
 import * as Dialog from '@radix-ui/react-dialog'
+import OrderHistoryTable from "./components/OrderHistoryTable"
+import * as Popover from '@radix-ui/react-popover'
+import { Ellipsis } from 'lucide-react'
 
 const page = () => {
 
     const pdfPath = process.env.NEXT_PUBLIC_ORDER_REPORT
 
     const [showItemDetail, setShowItemDetails] = useState(false)
-
     const [collectionItem, setCollectionItem] = useState(null)
+
+    const [showDatePopover, setShowDatePopover] = useState(false)
+    const [dateFilter, setDateFilter] = useState({
+        fromDate: "",
+        toDate: ""
+    })
     const { orderDetails, errorOrder, userOrder, latestPdf } = useGetOrderList()
 
     console.log(pdfPath, latestPdf)
@@ -23,59 +31,62 @@ const page = () => {
             <Header />
 
             <main className=' container w-full my-5'>
-                <div className='flex justify-end items-center mb-7'>
-                    <a href={latestPdf ? `${pdfPath}/${latestPdf}` : "#"} target={latestPdf ? "_blank" : "_self"} className='px-2 py-1 border-2 rounded-md flex gap-1 items-center text-base font-medium hover:border-black/60'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="#ef5350" d="M13 9h5.5L13 3.5zM6 2h8l6 6v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2m4.93 10.44c.41.9.93 1.64 1.53 2.15l.41.32c-.87.16-2.07.44-3.34.93l-.11.04l.5-1.04c.45-.87.78-1.66 1.01-2.4m6.48 3.81c.18-.18.27-.41.28-.66c.03-.2-.02-.39-.12-.55c-.29-.47-1.04-.69-2.28-.69l-1.29.07l-.87-.58c-.63-.52-1.2-1.43-1.6-2.56l.04-.14c.33-1.33.64-2.94-.02-3.6a.85.85 0 0 0-.61-.24h-.24c-.37 0-.7.39-.79.77c-.37 1.33-.15 2.06.22 3.27v.01c-.25.88-.57 1.9-1.08 2.93l-.96 1.8l-.89.49c-1.2.75-1.77 1.59-1.88 2.12c-.04.19-.02.36.05.54l.03.05l.48.31l.44.11c.81 0 1.73-.95 2.97-3.07l.18-.07c1.03-.33 2.31-.56 4.03-.75c1.03.51 2.24.74 3 .74c.44 0 .74-.11.91-.3m-.41-.71l.09.11c-.01.1-.04.11-.09.13h-.04l-.19.02c-.46 0-1.17-.19-1.9-.51c.09-.1.13-.1.23-.1c1.4 0 1.8.25 1.9.35M7.83 17c-.65 1.19-1.24 1.85-1.69 2c.05-.38.5-1.04 1.21-1.69zm3.02-6.91c-.23-.9-.24-1.63-.07-2.05l.07-.12l.15.05c.17.24.19.56.09 1.1l-.03.16l-.16.82z" />
-                    </svg>
-                        download
-                    </a>
-                </div>
-                <div className="overflow-x-auto custom-scroll  h-fit max-h-[600px] overflow-y-auto">
-                    <table className="w-full">
-                        <thead className="bg-gray-50">
-                            <tr>
-                                <th className="py-4 px-8 text-start /70 font-medium">Name</th>
-                                <th className="py-4 px-8 text-start /70 font-medium">Address</th>
-                                <th className="py-4 px-8 text-start /70 font-medium">Items</th>
-                                <th className="py-4 px-8 text-start /70 font-medium">Total Amount</th>
-                                <th className="py-4 px-8 text-start /70 font-medium">Date</th>
-                                <th className="py-4 px-8 text-start /70 font-medium">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {orderDetails
+                <section className='flex justify-end items-center mb-7 gap-5'>
+                    <div className=' cursor-pointer' onClick={() => setShowDatePopover(!showDatePopover)}>
+                        <Popover.Root open={showDatePopover} onOpenChange={setShowDatePopover}>
+                            <Popover.Trigger asChild >
+                                <svg xmlns="http://www.w3.org/2000/svg" className='border-2 border-[#253acd] rounded-md' width="28" height="28" viewBox="0 0 24 24">
+                                    <path fill="#253acd" d="M22 18.605a.75.75 0 0 1-.75.75h-5.1a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h7.74a2.93 2.93 0 0 1 5.66 0h5.1a.75.75 0 0 1 .75.75m0-13.21a.75.75 0 0 1-.75.75H18.8a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h10.39a2.93 2.93 0 0 1 5.66 0h2.45a.74.74 0 0 1 .75.75m0 6.6a.74.74 0 0 1-.75.75H9.55a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h1.14a2.93 2.93 0 0 1 5.66 0h11.7a.75.75 0 0 1 .75.75" />
+                                </svg>
+                            </Popover.Trigger>
+                            <Popover.Portal>
+                                <Popover.Content
+                                    className="w-[250px] rounded bg-white border-white p-2 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] will-change-[transform,opacity] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=top]:animate-slideDownAndFade"
+                                    sideOffset={5}
+                                >
+                                    <div className="flex flex-col gap-1 border-none ">
+                                        <div className='flex gap-2 items-center justify-end'>
+                                            <Popover.Close
 
 
-                                && orderDetails.map((item, index) => (
-                                    <tr key={index}>
-                                        <td className="border-t py-4 px-8 border-gray-100">{`${item.firstName} ${item.lastName}`}</td>
-                                        <td className="border-t py-4 px-8 border-gray-100">{item.streetAddress}</td>
-                                        <td className="border-t py-4 px-8 border-gray-100 cursor-pointer" onClick={() => { setShowItemDetails(!showItemDetail), setCollectionItem(item.foodItems) }}>{item?.foodItems?.length}</td>
-                                        <td className="border-t py-4 px-8 border-gray-100">{item.amount}</td>
-                                        <td className="border-t py-4 px-8 border-gray-100">{item.date.split("T")[0]}</td>
-                                        <td className="relative border-t py-4 px-8 border-gray-100" >
-                                            <h1 className={`py-[4px] px-2 w-fit rounded-lg font-medium cursor-pointer ${item.status.toLowerCase() === "pending" ? "border border-orange-400 text-orange-400" : item.status.toLowerCase() === "delivered" ? "border border-green-500 text-green-500" : "border border-red-500 text-red-500"}`}
-                                            // onClick={() =>
-                                            //     setShowStatusUpdate(index)
-                                            // }
+                                                aria-label="Close"
+                                            >
+                                                <button className='text-lg font-medium text-gray-700'>
+                                                    Apply
+                                                </button>
+                                            </Popover.Close>
+                                            <Popover.Close
 
-                                            >{item.status}</h1>
-                                            {/* {showStatusUpdate === index && <div ref={statusClose} className='absolute top-[80%] bg-white shadow-lg z-[10] border border-white rounded-md max-h-[120px]  oveflow-y-auto flex flex-col gap-1 w-[60%]'>
-                                                {status.map((element, index) => (
-                                                    <h1 key={index} className='px-3 py-[4px] hover:bg-blue-50' onClick={() => { getOrderUpdate(item._id, element) }}>{element}</h1>
+                                                aria-label="Close"
+                                            >
+                                                <button className='border rounded-md px-5 py-1 bg-blue-700 text-white hover:bg-blue-600'>
+                                                    Close
+                                                </button>
 
-                                                ))}
-                                            </div>} */}
-                                        </td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-                </div>
+                                            </Popover.Close>
+                                        </div>
+
+
+                                    </div>
+
+                                </Popover.Content>
+                            </Popover.Portal>
+                        </Popover.Root>
+                    </div>
+                    <div>
+                        <a href={latestPdf ? `${pdfPath}/${latestPdf}` : "#"} target={latestPdf ? "_blank" : "_self"} className='px-2 py-1 border-2 rounded-md flex gap-1 items-center text-base font-medium hover:border-black/60'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="#ef5350" d="M13 9h5.5L13 3.5zM6 2h8l6 6v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2m4.93 10.44c.41.9.93 1.64 1.53 2.15l.41.32c-.87.16-2.07.44-3.34.93l-.11.04l.5-1.04c.45-.87.78-1.66 1.01-2.4m6.48 3.81c.18-.18.27-.41.28-.66c.03-.2-.02-.39-.12-.55c-.29-.47-1.04-.69-2.28-.69l-1.29.07l-.87-.58c-.63-.52-1.2-1.43-1.6-2.56l.04-.14c.33-1.33.64-2.94-.02-3.6a.85.85 0 0 0-.61-.24h-.24c-.37 0-.7.39-.79.77c-.37 1.33-.15 2.06.22 3.27v.01c-.25.88-.57 1.9-1.08 2.93l-.96 1.8l-.89.49c-1.2.75-1.77 1.59-1.88 2.12c-.04.19-.02.36.05.54l.03.05l.48.31l.44.11c.81 0 1.73-.95 2.97-3.07l.18-.07c1.03-.33 2.31-.56 4.03-.75c1.03.51 2.24.74 3 .74c.44 0 .74-.11.91-.3m-.41-.71l.09.11c-.01.1-.04.11-.09.13h-.04l-.19.02c-.46 0-1.17-.19-1.9-.51c.09-.1.13-.1.23-.1c1.4 0 1.8.25 1.9.35M7.83 17c-.65 1.19-1.24 1.85-1.69 2c.05-.38.5-1.04 1.21-1.69zm3.02-6.91c-.23-.9-.24-1.63-.07-2.05l.07-.12l.15.05c.17.24.19.56.09 1.1l-.03.16l-.16.82z" />
+                        </svg>
+                            download
+                        </a>
+                    </div>
+                </section>
+                <section className="overflow-x-auto custom-scroll  h-fit max-h-[600px] overflow-y-auto">
+                    <OrderHistoryTable orderDetails={orderDetails} />
+                </section>
                 {showItemDetail && <Dialog.Root onOpenChange={setShowItemDetails} open={showItemDetail}>
                     <Dialog.Trigger asChild>
                         <button className="inline-flex h-[35px] items-center justify-center rounded bg-violet4 px-[15px] font-medium leading-none text-violet11 outline-none outline-offset-1 hover:bg-mauve3 focus-visible:outline-2 focus-visible:outline-violet6 select-none">
-                            {/* Edit profile */}
                         </button>
                     </Dialog.Trigger>
                     <Dialog.Portal>
@@ -84,7 +95,6 @@ const page = () => {
                             <Dialog.Title >
 
                             </Dialog.Title>
-
 
                             <div className="flex flex-col gap-4 px-[30px] py-4">
                                 <h1 className='text-center font-medium text-base text-gray-700 mb-1'>Food Details</h1>
