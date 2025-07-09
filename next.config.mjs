35a980524d0774b61ed9d4b461f6a99514217be3
aashish <shahaashih@gmail.com>
2025-07-09 18:03:55 +0545

    image domain changed

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     ];
   },
   images: {
-    domains: ["localhost", "https://backend-chito-khaja.onrender.com"],
+    domains: ["localhost", "backend-chito-khaja.onrender.com"],
   },
 };
 
